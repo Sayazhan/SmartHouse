fb7959a911460ef448e421b5b58e313b43b2e6f9
Sayazhan <sayazhansaiko@gmail.com>
2015-05-19 13:42:08 +0200

    Temperature activity

---
+++ src/kz/softeng/sdu/SmartHouse/Temperature.java
@@ -0,0 +1,15 @@
+package kz.softeng.sdu.SmartHouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sayazhan on 19.05.2015.
+ */
+public class Temperature extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temperature);
+    }
+}
